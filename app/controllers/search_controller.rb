6c7c4a6d5243ecbf56e7b646984e7378594e3ba9
foymikek <>
2021-02-12 10:05:07 -0700

    add succesful endpoint connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
 class SearchController < ApplicationController
   def index
+    param = params[:nation]
+    param = param.gsub("_", " ")
+    connection = Faraday.new("https://last-airbender-api.herokuapp.com")
+    response = connection.get("/api/v1/characters?affiliation=#{param}")
+    results = JSON.parse(response.body, symbolize_names: true)
   end
 end
