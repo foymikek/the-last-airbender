fee3f41e7efafa721324238047d26781b61a983f
foymikek <>
2021-02-12 10:23:18 -0700

    Add passing feature test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,11 @@ class SearchController < ApplicationController
     param = params[:nation]
     param = param.gsub("_", " ")
     connection = Faraday.new("https://last-airbender-api.herokuapp.com")
-    response = connection.get("/api/v1/characters?affiliation=#{param}")
+    response = connection.get("/api/v1/characters?affiliation=#{param}&perPage=100&page=1")
     results = JSON.parse(response.body, symbolize_names: true)
+    @characters = results.map do |result|
+      Character.new(result)
+    end
+    # require 'pry'; binding.pry
   end
 end
--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -17,8 +17,8 @@ RSpec.describe 'search', type: :feature do
       expect(current_path).to eq(search_path)
 
       # Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
-      expect(page).to have_content("Population: 100")
-      expect(page).to have_css('.community', count: 100)
+      expect(page).to have_content("Population: 97")
+      expect(page).to have_css('.community', count: 97)
 
       # And I should see a list with the detailed information for the first 25 members of the Fire Nation.
       # And for each of the members I should see:
@@ -28,9 +28,10 @@ RSpec.describe 'search', type: :feature do
       # - Any affiliations that the member has
       within(first('.community')) do
         expect(page).to have_css('.name')
+        expect(page).to have_css('.photo')
         expect(page).to have_css('.allies')
         expect(page).to have_css('.enemies')
-        expect(page).to have_css('.affiliations')
+        expect(page).to have_css('.affiliation')
       end
 
     end
